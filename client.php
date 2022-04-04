36ba3131e560dd4f0577402f47ff9890293f2247
Vlasta Neubauer <cz.paranoiq@gmail.com>
2022-04-04 16:33:05 +0200

    Fix lint failing on files for different PHP versions

--- client.php
+++ client.php
@@ -105,9 +105,9 @@ if (!class_exists(Debugger::class)) {
     require_once __DIR__ . '/src/interceptors/SyslogInterceptor.php';
     // function signature changed
     if (PHP_VERSION_ID < 80000) {
-        require_once __DIR__ . '/src/interceptors/FakePdo.php';
+        require_once __DIR__ . '/src7/FakePdo7.php';
     } else {
-        require_once __DIR__ . '/src/interceptors/FakePdo8.php';
+        require_once __DIR__ . '/src8/FakePdo8.php';
     }
 
     require_once __DIR__ . '/src/stream-wrappers/StreamWrapper.php';
