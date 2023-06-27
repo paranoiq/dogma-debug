8ad1424e764b293a7987e0e0dbc2aa3b174e9cce
paranoiq <cz.paranoiq@gmail.com>
2023-06-27 20:31:16 +0200

    Spell check

--- src/interceptors/CurlInterceptor.php
+++ src/interceptors/CurlInterceptor.php
@@ -7,6 +7,8 @@
  * For the full copyright and license information read the file 'license.md', distributed with this source code
  */
 
+// spell-check-ignore: INIFILE KEYPASSWD KRBLEVEL
+
 namespace Dogma\Debug;
 
 use CurlHandle;
