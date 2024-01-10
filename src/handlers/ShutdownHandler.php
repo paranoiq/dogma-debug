3b4e0165587fceb86cbbe080e5c8ff11476345af
Vlasta Neubauer <cz.paranoiq@gmail.com>
2024-01-10 21:58:23 +0100

    Fix hinting signal names when ShutdownHandler is disabled

--- src/handlers/ShutdownHandler.php
+++ src/handlers/ShutdownHandler.php
@@ -56,7 +56,7 @@ use const SIGXFSZ;
  *
  * PHP request shutdown steps:
  * - call all functions registered via register_shutdown_function()
- * - call all* __destruct() methods
+ * - call all __destruct() methods
  * - empty all output buffers
  * - end all PHP extensions (e.g. sessions)
  * - turn off output layer (send HTTP headers, terminate output handlers etc.)
@@ -103,53 +103,9 @@ class ShutdownHandler
         }
 
         if (function_exists('pcntl_signal')) {
-            self::$signals = [
-                // (~ system depend., * core dump) N Action       Description (Synonym)
-                'hangup' => SIGHUP,           //   1 Terminate    Hangup
-                'interrupt' => SIGINT,        //   2 Terminate    Terminal interrupt signal
-                'quit' => SIGQUIT,            //   3 Terminate*   Terminal quit signal
-                'illegal' => SIGILL,          //   4 Terminate*   Illegal instruction
-                'trace' => SIGTRAP,           //   5 Terminate*   Trace/breakpoint trap
-                'abort' => SIGABRT,           //   6 Terminate*   Process abort signal (SIGIOT)
-                'bus_error' => SIGBUS,        //  ~7 Terminate*   Access to an undefined portion of a memory object
-                'fpe' => SIGFPE,              //   8 Terminate*   Erroneous arithmetic operation
-                'kill' => SIGKILL,            //   9 Terminate    Kill (cannot be caught or ignored)
-                'user_1' => SIGUSR1,          // ~10 Terminate    User-defined signal 1
-                'segfault' => SIGSEGV,        //  11 Terminate*   Invalid memory reference
-                'user_2' => SIGUSR2,          // ~12 Terminate    User-defined signal 2
-                'pipe' => SIGPIPE,            //  13 Terminate    Write on a pipe with no one to read it
-                'alarm' => SIGALRM,           //  14 Terminate    Alarm clock
-                'terminate' => SIGTERM,       //  15 Terminate    Termination signal
-                'stack_fault' => SIGSTKFLT,   // ~16 Terminate    Stack fault on coprocessor
-                'child' => SIGCHLD,           // ~17 Ignore       Child process terminated, stopped, or continued (SIGCLD)
-                'continue' => SIGCONT,        // ~18 Continue     Continue executing, if stopped
-                'stop' => SIGSTOP,            // ~19 Stop         Stop executing (cannot be caught or ignored)
-                'term_stop' => SIGTSTP,       // ~20 Stop         Terminal stop signal
-                'term_input' => SIGTTIN,      // ~21 Stop         Background process attempting read
-                'term_output' => SIGTTOU,     // ~22 Stop         Background process attempting write
-                'urgent' => SIGURG,           // ~23 Ignore       Out-of-band data is available at a socket
-                'cpu' => SIGXCPU,             // ~24 Terminate*   CPU time limit exceeded
-                'file_size' => SIGXFSZ,       // ~25 Terminate*   File size limit exceeded
-                'virtual_alarm' => SIGVTALRM, // ~26 Terminate    Virtual timer expired
-                'profiling' => SIGPROF,       // ~27 Terminate    Profiling timer expired
-                'window_change' => SIGWINCH,  // ~28 Ignore       Terminal window size changed
-                'io' => SIGIO,                // ~29 Terminate    I/O now possible (SIGPOLL)
-                'power' => SIGPWR,            // ~30 Terminate    Power failure (SIGINFO)
-                'system_call' => SIGSYS,      // ~31 Terminate*   Bad system call (SIGUNUSED)
-            ];
-            self::$nonTerminating = [
-                self::$signals['child'],
-                self::$signals['stop'],
-                self::$signals['term_stop'],
-                self::$signals['term_input'],
-                self::$signals['term_output'],
-                self::$signals['urgent'],
-                self::$signals['window_change'],
-            ];
-            self::$ignore = [
-                // signal from finished processes started via exec() etc.
-                self::$signals['child'],
-            ];
+            if (self::$signals === []) {
+                self::init();
+            }
 
             // cannot set handler
             $ignoreSignals[] = self::$signals['kill'];
@@ -175,6 +131,61 @@ class ShutdownHandler
         self::$enabled = true;
     }
 
+    private static function init(): void
+    {
+        if (function_exists('pcntl_signal')) {
+            return;
+        }
+
+        self::$signals = [
+            // (~ system depend., * core dump) N Action       Description (Synonym)
+            'hangup' => SIGHUP,           //   1 Terminate    Hangup
+            'interrupt' => SIGINT,        //   2 Terminate    Terminal interrupt signal
+            'quit' => SIGQUIT,            //   3 Terminate*   Terminal quit signal
+            'illegal' => SIGILL,          //   4 Terminate*   Illegal instruction
+            'trace' => SIGTRAP,           //   5 Terminate*   Trace/breakpoint trap
+            'abort' => SIGABRT,           //   6 Terminate*   Process abort signal (SIGIOT)
+            'bus_error' => SIGBUS,        //  ~7 Terminate*   Access to an undefined portion of a memory object
+            'fpe' => SIGFPE,              //   8 Terminate*   Erroneous arithmetic operation
+            'kill' => SIGKILL,            //   9 Terminate    Kill (cannot be caught or ignored)
+            'user_1' => SIGUSR1,          // ~10 Terminate    User-defined signal 1
+            'segfault' => SIGSEGV,        //  11 Terminate*   Invalid memory reference
+            'user_2' => SIGUSR2,          // ~12 Terminate    User-defined signal 2
+            'pipe' => SIGPIPE,            //  13 Terminate    Write on a pipe with no one to read it
+            'alarm' => SIGALRM,           //  14 Terminate    Alarm clock
+            'terminate' => SIGTERM,       //  15 Terminate    Termination signal
+            'stack_fault' => SIGSTKFLT,   // ~16 Terminate    Stack fault on coprocessor
+            'child' => SIGCHLD,           // ~17 Ignore       Child process terminated, stopped, or continued (SIGCLD)
+            'continue' => SIGCONT,        // ~18 Continue     Continue executing, if stopped
+            'stop' => SIGSTOP,            // ~19 Stop         Stop executing (cannot be caught or ignored)
+            'term_stop' => SIGTSTP,       // ~20 Stop         Terminal stop signal
+            'term_input' => SIGTTIN,      // ~21 Stop         Background process attempting read
+            'term_output' => SIGTTOU,     // ~22 Stop         Background process attempting write
+            'urgent' => SIGURG,           // ~23 Ignore       Out-of-band data is available at a socket
+            'cpu' => SIGXCPU,             // ~24 Terminate*   CPU time limit exceeded
+            'file_size' => SIGXFSZ,       // ~25 Terminate*   File size limit exceeded
+            'virtual_alarm' => SIGVTALRM, // ~26 Terminate    Virtual timer expired
+            'profiling' => SIGPROF,       // ~27 Terminate    Profiling timer expired
+            'window_change' => SIGWINCH,  // ~28 Ignore       Terminal window size changed
+            'io' => SIGIO,                // ~29 Terminate    I/O now possible (SIGPOLL)
+            'power' => SIGPWR,            // ~30 Terminate    Power failure (SIGINFO)
+            'system_call' => SIGSYS,      // ~31 Terminate*   Bad system call (SIGUNUSED)
+        ];
+        self::$nonTerminating = [
+            self::$signals['child'],
+            self::$signals['stop'],
+            self::$signals['term_stop'],
+            self::$signals['term_input'],
+            self::$signals['term_output'],
+            self::$signals['urgent'],
+            self::$signals['window_change'],
+        ];
+        self::$ignore = [
+            // signal from finished processes started via exec() etc.
+            self::$signals['child'],
+        ];
+    }
+
     public static function disable(): void
     {
         if (function_exists('pcntl_signal')) {
@@ -198,6 +209,10 @@ class ShutdownHandler
 
     public static function getSignalName(int $signal): string
     {
+        if (self::$signals === [] && function_exists('pcntl_signal')) {
+            self::init();
+        }
+
         return array_search($signal, self::$signals, true) ?: (string) $signal;
     }
 
@@ -209,8 +224,6 @@ class ShutdownHandler
         $name = self::getSignalName($signal);
 
         if (!in_array($signal, self::$nonTerminating, true)) {
-            //Debugger::send(Message::ERROR, Ansi::white(" Terminated by $name signal. ", Ansi::DRED));
-
             Debugger::setTermination("signal ({$name})");
             exit;
         } elseif (!in_array($signal, self::$ignore, true)) {
@@ -227,8 +240,6 @@ class ShutdownHandler
             : ($signal === PHP_WINDOWS_EVENT_CTRL_BREAK ? 'ctrl-break' : 'unknown');
 
         Debugger::setTermination("signal ({$name})");
-
-        //Debugger::send(Message::ERROR, Ansi::white(" Terminated by {$name} signal. ", Ansi::DRED));
         exit;
     }
 
